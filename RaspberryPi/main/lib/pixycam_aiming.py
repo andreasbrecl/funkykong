1513856235cc1c7b881b18066cc51b02fdaa85a7
andreasbrecl <andreas@brecl.com>
2022-11-07 17:23:07 -0700

    removed file sys import

--- RaspberryPi/main/lib/pixycam_aiming.py
+++ RaspberryPi/main/lib/pixycam_aiming.py
@@ -5,8 +5,6 @@ Date: 11/07/2022
 This class will deal with pulling 
 """
 
-import sys
-
 class PixycamAiming:
     def __init__(self):
-        sys.path.append("~/Documents/Code/Pixy2Python/")
+        pass
