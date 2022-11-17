040fdcc26535a9439a6569c7942bb75b14ffa4d6
Sean Newman <49414106+SeanHNewman@users.noreply.github.com>
2022-11-16 17:03:16 -0700

    g

--- RaspberryPi/main/main.py
+++ RaspberryPi/main/main.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 #!/usr/bin/env python3
 """
 Author: Andrea Brecl
@@ -12,6 +13,31 @@ to put the system into the correct location.
 
 # Import classes
 from lib.uart_comms import UARTComms
+
+
+import sys
+# sys.path.append("~/Documents/Code/Pixy2Python/pixy2/build/python_demos")
+import pixy 
+from ctypes import *
+from pixy import *
+
+# Pixy2 Python SWIG get blocks example #
+
+# print("Pixy2 Python SWIG Example -- Get Blocks")
+
+pixy.init ()
+pixy.change_prog ("color_connected_components");
+
+class Blocks (Structure):
+  _fields_ = [ ("m_signature", c_uint),
+    ("m_x", c_uint),
+    ("m_y", c_uint),
+    ("m_width", c_uint),
+    ("m_height", c_uint),
+    ("m_angle", c_uint),
+    ("m_index", c_uint),
+    ("m_age", c_uint) ]
+
 
 
 def executeFunctions():
@@ -44,6 +70,11 @@ def main():
 
     Output: None
     """
+
+
+    blocks = BlockArray(1)
+    frame = 0
+    
     # Execute main functionaliy
     executeFunctions()
 
