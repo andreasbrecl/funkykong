1797130f4a7334e31b034ec87125046341d5e6dd
andreasbrecl <andreas@brecl.com>
2022-11-29 16:11:03 -0700

    updated test script

--- RaspberryPi/main/test.py
+++ RaspberryPi/main/test.py
@@ -10,7 +10,15 @@ def main():
 
     Output: None
     """
-    # Define pins
+    # Define GPIO pins
+    firePin = 17
+    reloadPin = 27
+    reloadDonePin = 22
+    startPin = 23
+    stopPin = 24
+
+    # Logic values
+    reloadDone = 0
 
     # Define pin type
     GPIO.setmode(GPIO.BCM)
@@ -19,6 +27,33 @@ def main():
     GPIO.setup(reloadDonePin, GPIO.IN)
 
     while True:
+
+        # Check start pin
+        if GPIO.input(startPin):
+
+            # Print that the pin has started
+            print("Start Pin Pressed")
+
+        # Stop pin
+        if GPIO.input(stopPin):
+
+            # Print pin is pressed'
+            print("Stop Pin Pressed")
+
+        # Check if reload pin is triggerd
+        if GPIO.input(reloadPin) == False and reloadDone == 0:
+            GPIO.output(firePin, 1)
+            print("Shooting")
+        else:
+            GPIO.output(firePin, 0)
+            print("Reloading")
+            reloadDone = 1
+
+        if GPIO.input(reloadDonePin):
+
+            # Reloaded
+            print("Reloaded")
+            reloadDone = 0
         
 
 if __name__ == "__main__":
