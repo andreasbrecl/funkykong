2256ce861733cca7722d5404d5ebd4cf1fb62e2d
andreasbrecl <andreas@brecl.com>
2022-11-28 19:52:24 -0700

    updated percentage

--- RaspberryPi/main/lib/path_planning.py
+++ RaspberryPi/main/lib/path_planning.py
@@ -802,7 +802,7 @@ class PathPlanning:
         percentDifference = self.calculatePercentage(ultrasonic1, ultrasonic2)
 
         # See if value is within 5 %
-        if percentDifference <= 10 and ultrasonic1 < 25 and ultrasonic2 < 25:
+        if percentDifference <= 5 and ultrasonic1 < 25 and ultrasonic2 < 25:
 
             # Stop motion
             movementCommand = stop
