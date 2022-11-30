e548c4c2f647bd4b02eb4853d7b38fa98059c9f1
andreasbrecl <andreas@brecl.com>
2022-11-29 22:06:39 -0700

    updated timing

--- RaspberryPi/main/main.py
+++ RaspberryPi/main/main.py
@@ -41,8 +41,8 @@ def executeFunctions():
     stopPin = 24
 
     # Path planning variables, times are in seconds
-    diagTimeToShooting = 15
-    leftRightTimeToShooting = 5
+    diagTimeToShooting = 12
+    leftRightTimeToShooting = 7
     forwardTimeToShooting = 2
     timeList = [diagTimeToShooting, leftRightTimeToShooting, forwardTimeToShooting]
     
