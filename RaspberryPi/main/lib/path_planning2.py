afeee293d2f3a8ef640718bb7e0bca9512d1f157
andreasbrecl <andreas@brecl.com>
2022-12-02 16:59:44 -0700

    added check

--- RaspberryPi/main/lib/path_planning2.py
+++ RaspberryPi/main/lib/path_planning2.py
@@ -852,7 +852,7 @@ class PathPlanning2:
             movementCommand = backwards
 
             # Check what side the robot is on
-            if sideColor == colorCheckGreen and (lineSensorReading3 == 1 or lineSensorReading4 == 1):
+            if lineSensorReading3 == 1 or lineSensorReading4 == 1:
 
                 if lineSensorReading1 == 1 and lineSensorReading2 == 1 and lineSensorReading3 == 1 and lineSensorReading4 == 1:
 
